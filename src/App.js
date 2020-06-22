1e8e594784b74600124c03b3c8c38b05f895756c
Dhanushka Nadeeshan <36134983+DhanushkaNadeeshan@users.noreply.github.com>
2020-06-22 21:08:35 +0530

    Update App.js
    Testing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>React App</h2>
+          <h2>Appceylon</h2>
         </div>
         <BrowserRouter>
           <div>
